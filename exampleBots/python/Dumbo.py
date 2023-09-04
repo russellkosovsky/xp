9dc64b1d9fefe743b7e8c0e3fa94ecea59f37ebc
russellkosovsky <russellkosovsky@gmail.com>
2023-09-04 12:10:09 -0400

    hi

--- exampleBots/python/Dumbo.py
+++ exampleBots/python/Dumbo.py
@@ -1,27 +1,27 @@
-#Evan Gray - January 2018
 import libpyAI as ai
 def AI_loop():
-  #Release keys
-  ai.thrust(0)
-  ai.turnLeft(0)
-  ai.turnRight(0)
-  #Set variables
-  heading = int(ai.selfHeadingDeg())
-  tracking = int(ai.selfTrackingDeg())
-  frontWall = ai.wallFeeler(500,heading)
-  leftWall = ai.wallFeeler(500,heading+5)
-  rightWall = ai.wallFeeler(500,heading-5)
-  trackWall = ai.wallFeeler(500,tracking)
-  #Thrust rules
-  if ai.selfSpeed() <= 50 and frontWall >= 5:
-    ai.thrust(1)
-  elif trackWall < 5:
-    ai.thrust(1)
-  #Turn rules
-  if leftWall < rightWall:
-    ai.turnRight(1)
-  else:
-    ai.turnLeft(1)
-  #Just keep shooting
-  ai.fireShot()
-ai.start(AI_loop,["-name","Dumbo","-join","localhost"])
+    #Release keys
+    ai.thrust(0)
+    ai.turnLeft(0)
+    ai.turnRight(0)
+    #Set variables
+    heading = int(ai.selfHeadingDeg())
+    tracking = int(ai.selfTrackingDeg())
+    frontWall = ai.wallFeeler(1000, heading)
+    leftWall = ai.wallFeeler(1000, heading - 90)
+    rightWall = ai.wallFeeler(1000, heading + 90)
+    backWall = ai.wallFeeler(1000, heading -180)
+    trackWall = ai.wallFeeler(1000, tracking)
+    #Thrust rules
+    if ai.selfSpeed() <= 5 and frontWall >= 500:
+        ai.thrust(1)
+    elif backWall < 350 and ai.wallFeeler(500, heading) == 500:
+        ai.thrust(1)
+    #Turn rules
+    if leftWall > rightWall:
+        ai.turnRight(1)
+    else:
+        ai.turnLeft(1)
+    #Just keep shooting
+    ai.fireShot()
+ai.start(AI_loop,["-name","Dumbo"])
